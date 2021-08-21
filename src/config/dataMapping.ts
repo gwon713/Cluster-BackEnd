0272040f06f3f0e10dfe356f7c5c4e0ac0935e4a
Gwon Min Je <gwon713@gmail.com>
2021-08-21 21:57:42 +0900

    feat : data Mapping add

---
+++ src/config/dataMapping.ts
@@ -0,0 +1,24 @@
+import { User } from '../models/user';
+import { Group } from '../models/group/group';
+import { GroupLike } from '../models/group/group_like';
+const userJSONData = require('../../data/user.json');
+
+export const ClassData_User = (): User[]=>{
+    let UserData : User[] = []; 
+    userJSONData.forEach((item: { seq: Number, email: String, nickname: String, token: String, social: String, file: String, login_time: String, edit_time: String, join_time: String, deleted: Boolean }) => {
+        UserData.push(new User(item.seq, item.email, item.nickname, item.token, item.social, item.file, item.login_time, item.edit_time, item.join_time, item.deleted));
+    });
+    return UserData;
+}
+
+export const ClassData_Group = (): Group[]=>{
+    let GroupData : Group[] = []; 
+
+    return GroupData;
+}
+
+export const ClassData_GrouLike = (): GroupLike[]=>{
+    let GroupLikeData : GroupLike[] = []; 
+
+    return GroupLikeData;
+}
