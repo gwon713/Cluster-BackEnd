28d135a1ece23bf5c82181ffa3ce6d2dcf6c26b3
Gwon Min Je <gwon713@gmail.com>
2021-08-29 00:45:41 +0900

    feat: pm2 node env 설정 추가

--- serversystem.config.js
+++ serversystem.config.js
@@ -1,11 +1,14 @@
 module.exports = {
     apps: [{
-    name: 'ddudo_BE',
-    script: './build/start.js',
-    instances: 0,
-    exec_mode: 'cluster',
-    wait_ready: true,
-    listen_timeout: 50000,
-    kill_timeout: 5000
+        name: 'ddudo_BE',
+        env: {
+            "NODE_ENV": "dev"
+        },
+        script: './build/start.js',
+        instances: 0,
+        exec_mode: 'cluster',
+        wait_ready: true,
+        listen_timeout: 50000,
+        kill_timeout: 5000
     }]
 }
