bbd0ef6517ad6b7625d62d0d49852d2b5aad36d1
Gwon Min Je <gwon713@gmail.com>
2021-09-08 22:58:59 +0900

    feat: process setting

--- src/start.ts
+++ src/start.ts
@@ -5,10 +5,27 @@ try {
     const app = new App().app;
     const port : number = 3000;
 
-    app.listen(port, () => {
+    const setProcess = ()=>{
+        process.on('SIGINT', ()=>{
+            logger.error('SIGINT ERROR');
+            listeningServer.close(()=>{
+                logger.info('##### SERVER IS CLOSE #####');
+                process.exit(0);
+            });
+        });
+        process.on('uncaughtException', (err)=>{ // 못잡은 error handler
+            logger.error('uncaughtException : '+err.stack);
+            listeningServer.close(()=>{
+                logger.info("##### SERVER IS CLOSE #####");
+                process.exit(0);
+            })
+        }); 
+    }
+    let listeningServer = app.listen(port, () => {
         logger.info(` NODE ENV = ${process.env.NODE_ENV} `)
         logger.info(`Server is running on port : ${port}`);
     });
+    setProcess();
 } catch(err) {
     logger.error(err);
 }
