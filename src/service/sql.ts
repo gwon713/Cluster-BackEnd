b68ff1b5220cbf97722cc4933b95c3f74e2ff6d1
Gwon Min Je <gwon713@gmail.com>
2021-09-14 00:04:34 +0900

    fix: sql dir change & add user sql

---
+++ src/service/sql.ts
@@ -0,0 +1,4 @@
+export const search_User_SQL = 'select * from d_user where user_email = $1 and user_token = $2 and user_social = $3 and user_deleted = false'
+
+export const add_User_SQL = 'insert into user (user_email, user_nickname, user_token, user_social, user_file)\
+                            values ($1, $2, $3, $4, $5)'
