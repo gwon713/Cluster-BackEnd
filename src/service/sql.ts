b5caf69af61742560772a12712d1ba6c103cf408
Gwon Min Je <gwon713@gmail.com>
2021-09-22 22:45:43 +0900

    style: sql

--- src/service/sql.ts
+++ src/service/sql.ts
@@ -1,6 +1,5 @@
 export const search_user_SQL = 'select * from d_user where user_email = $1 and user_token = $2 and user_social = $3 and user_deleted = false'
 
-export const add_user_SQL = 'insert into d_user (user_email, user_nickname, user_token, user_social, user_file)\
-                            values ($1, $2, $3, $4, $5)'
+export const add_user_SQL = 'insert into d_user (user_email, user_nickname, user_token, user_social, user_file) values ($1, $2, $3, $4, $5)'
 
 export const update_user_profile_SQL = 'update d_user set user_nickname = $1 where user_email = $2 and user_token = $3 and user_social = $4'
