fe691e06b90f9da12b4d1e1a1760e441a12c5166
Gwon Min Je <gwon713@gmail.com>
2021-10-06 22:59:08 +0900

    fix: delete json parsing

--- src/config/dataMapping.ts
+++ src/config/dataMapping.ts
@@ -1,24 +1,24 @@
-import { User } from '../models/user';
-import { Group } from '../models/group/group';
-import { GroupLike } from '../models/group/group_like';
-import { Code } from '../models/common/code';
-import { Code_detail } from '../models/common/code_detail';
+// import { User } from '../models/user';
+// import { Group } from '../models/group/group';
+// import { GroupLike } from '../models/group/group_like';
+// import { Code } from '../models/common/code';
+// import { Code_detail } from '../models/common/code_detail';
 
-export const ClassData_User = (params: Array<any>): User=>{
-    let Class_User = new User();
-    return Class_User;
-}
+// export const ClassData_User = (params: Array<any>): User=>{
+//     let Class_User = new User();
+//     return Class_User;
+// }
 
-export const ClassData_Group = (): Group=>{
-}
+// export const ClassData_Group = (): Group=>{
+// }
 
-export const ClassData_GroupLike = (): GroupLike=>{
-}
+// export const ClassData_GroupLike = (): GroupLike=>{
+// }
 
-export const ClassData_Code = (): Code=>{
+// export const ClassData_Code = (): Code=>{
 
-}
+// }
 
-export const ClassData_Code_detail = (): Code_detail=>{
+// export const ClassData_Code_detail = (): Code_detail=>{
 
-}
+// }
--- src/models/json_model/jsonModel_user.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-import path from "path";
-
-export const get_userJSONData = (): Object[] =>{
-    return JSON.parse(fs.readFileSync(path.join(__dirname, '../../../data/user.json'), 'utf8'));
-}
